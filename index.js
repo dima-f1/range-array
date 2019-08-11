df4043dd2dbfe8ea0475f4f4c490f53d75a9acb6
dima <dima.mandryka@gmail.com>
2019-08-12 00:37:43 +0300

    fix project structure

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-const rangeArray = (start = 1, end, step = 1, fn) => {
-    function* range(start, end, step, fn) {
-        while (start <= end) {
-            yield fn ? fn(start) : start;
-            start += step;
-        }
-    }
-
-    return Array.from(range(start, end, step, fn));
-};
-
-
-
-module.exports = rangeArray;
+module.exports = require('./src');
